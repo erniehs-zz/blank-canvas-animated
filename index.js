026c0206af936b40103de74ee9b1a07318c34825
ernie_hs <ernie_hs@hotmail.com>
2021-08-02 12:55:57 +0100

    cleanup and added a bit of css for the scrollbar

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-var ctx = document.getElementById("grid").getContext("2d")
+var ctx = document.getElementById("grid").getContext("2d");
 
 const resize = () => {
-    ctx.canvas.width = ctx.canvas.parentElement.clientWidth
-    ctx.canvas.height = ctx.canvas.parentElement.clientHeight
-}
+  ctx.canvas.width = ctx.canvas.parentElement.clientWidth;
+  ctx.canvas.height = ctx.canvas.parentElement.clientHeight;
+};
 
 const draw = () => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    ctx.beginPath()
-    ctx.strokeStyle = "red"
-    ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.moveTo(0, 0)
-    ctx.lineTo(ctx.canvas.width, ctx.canvas.height)
-    ctx.moveTo(0, ctx.canvas.height)
-    ctx.lineTo(ctx.canvas.width, 0)
-    ctx.stroke()
+  ctx.beginPath();
+  ctx.strokeStyle = "red";
+  ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
+  ctx.moveTo(0, ctx.canvas.height);
+  ctx.lineTo(ctx.canvas.width, 0);
+  ctx.stroke();
 
-    ctx.beginPath()
-    ctx.strokeStyle = "yellow"
-    ctx.rect(ball.x, ball.y, 20, 20)
-    ctx.stroke()
-}
+  ctx.beginPath();
+  ctx.strokeStyle = "yellow";
+  ctx.rect(ball.x, ball.y, 20, 20);
+  ctx.stroke();
+};
 
 window.addEventListener("load", () => {
-    resize()
-    draw()
-})
+  resize();
+  draw();
+});
 
 window.addEventListener("resize", () => {
-    resize()
-    draw() // TODO throttle
-})
+  resize();
+  draw(); // TODO throttle
+});
 
-var last = performance.now()
-var delta = 0
-var ball = { x: 100, y: 100 }
-var bv = { x: 10, y: -5 }
+var last = performance.now();
+var delta = 0;
+var ball = { x: 100, y: 100 };
+var bv = { x: 10, y: -5 };
 
 const update = (dt) => {
-    ball.x += bv.x
-    ball.y += bv.y
-    if (ball.x <= 0 || ball.x >= ctx.canvas.width) bv.x = -bv.x
-    if (ball.y <= 0 || ball.y >= ctx.canvas.height) bv.y = -bv.y
-}
+  ball.x += bv.x;
+  ball.y += bv.y;
+  if (ball.x <= 0 || ball.x >= ctx.canvas.width) bv.x = -bv.x;
+  if (ball.y <= 0 || ball.y >= ctx.canvas.height) bv.y = -bv.y;
+};
 
 const loop = (t) => {
-    window.requestAnimationFrame(loop)
-    delta = t - last
-    last = t
-    update(delta / 1000.0)
-    draw()
-}
+  window.requestAnimationFrame(loop);
+  delta = t - last;
+  last = t;
+  update(delta / 1000.0);
+  draw();
+};
 
-loop(performance.now())
+loop(performance.now());
